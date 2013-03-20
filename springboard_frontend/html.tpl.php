5f451993ec4e4bee029e0d41e8e2589f28f8b069
Sheena Donnelly <sheena.donnelly@gmail.com>
2013-03-20 18:29:54 -0400

    creating basic files for a frontend Springboard theme.

---
+++ springboard_frontend/html.tpl.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie ie7"> <![endif]-->
+<!--[if IE 8]>         <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie ie8"> <![endif]-->
+<!--[if IE 9]>         <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js"> <!--<![endif]-->
+  <head profile="<?php print $grddl_profile; ?>">
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>  
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    </div>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+  </body>
+</html>
---
+++ springboard_frontend/page.tpl.php
@@ -0,0 +1,20 @@
+<header class="clearfix">
+  <?php print theme_image($logo, $site_name, $site_name); ?>
+  <?php if($is_front){ ?><h1 id="site-name"><span><?php print($site_name); ?></span></h1><?php } else { print(l('<span>'.$site_name.'</span>', '<front>', array('html' => TRUE, 'attributes' => array('id' => 'site-name')))); } ?>
+  <?php print render($page['header']); ?>
+</header>
+<div id="main" class="clearfix">
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h1 class="page-title"><?php print $title; ?></h1>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php if($messages){ print $messages; } ?>
+  <?php print render($tabs); ?>        
+  <div class="element-invisible"><a id="main-content"></a></div>   
+  <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?> 
+  <?php print render($page['content']); ?>
+</div><!-- /main -->
+<footer id="footer" class="clearfix region">
+  <?php print render($page['footer']); ?>
+</footer>
