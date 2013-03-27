9e969fc025d9391792962a0c3b7f86a2e618566f
Sheena Donnelly <sheena.donnelly@gmail.com>
2013-03-27 17:36:05 -0400

    t0 - overriding theme_form_element, removing overrides for theme_hidden and theme_submit.

--- springboard_frontend/template.php
+++ springboard_frontend/template.php
@@ -15,15 +15,53 @@ function springboard_frontend_preprocess_page(&$variables) {
 }
 
 /**
- * Implements template_hidden().
+ * Overrides theme_form_element().
  */
-function springboard_frontend_hidden($variables) {
 
-}
+function springboard_frontend_form_element($variables) {
+  $element = &$variables['element'];
 
-/**
- * Implements template_submit().
- */
-function springboard_frontend_submit($variables) {
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
 
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? $element['#field_prefix'] : '';
+  $suffix = isset($element['#field_suffix']) ? $element['#field_suffix'] : '';
+
+  $output = '';
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if (!empty($element['#description'])) {
+    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+  return $output;
 }
