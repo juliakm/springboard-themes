1f2448156a3fcdec20ea7facdc5ae1799c349bf7
Patrick O'Brien <obrien317@gmail.com>
2013-03-27 15:15:37 -0500

    #no-ticket - small typo

--- springboard_frontend/template.php
+++ springboard_frontend/template.php
@@ -8,18 +8,22 @@ function springboard_frontend_preprocess_html(&$variables) {
 }
 
 /**
+ * Implements template_preprocess_page().
+ */
+function springboard_frontend_preprocess_page(&$variables) {
+
+}
+
+/**
  * Implements template_hidden().
  */
 function springboard_frontend_hidden($variables) {
-  $element = $variables['element'];
-  $element['#attributes']['type'] = 'hidden';
-  element_set_attributes($element, array('name', 'value'));
-  return '<input' . drupal_attributes($element['#attributes']) . " />\n";
+
 }
 
 /**
  * Implements template_submit().
  */
-function theme_submit($variables) {
-  return theme('button', $variables['element']);
+function springboard_frontend_submit($variables) {
+
 }
