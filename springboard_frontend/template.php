b671a61a68d696a7d1f56d2d69db74a3fd6ec1f7
Patrick O'Brien <obrien317@gmail.com>
2013-03-27 20:26:53 -0500

    #no-ticket - pushing template.php overrides

--- springboard_frontend/template.php
+++ springboard_frontend/template.php
@@ -65,3 +65,115 @@ function springboard_frontend_form_element($variables) {
 
   return $output;
 }
+
+/** 
+ * Overrides theme_form()
+ */
+function springboard_frontend_form($variables) {
+  $element = $variables['element'];
+  if (isset($element['#action'])) {
+    $element['#attributes']['action'] = drupal_strip_dangerous_protocols($element['#action']);
+  }
+  element_set_attributes($element, array('method', 'id'));
+  if (empty($element['#attributes']['accept-charset'])) {
+    $element['#attributes']['accept-charset'] = "UTF-8";
+  }
+  // Anonymous DIV to satisfy XHTML compliance.
+  return '<form' . drupal_attributes($element['#attributes']) . '>' . $element['#children'] . '</form>';
+}
+
+/** 
+ * Overrides theme_fieldset()
+ */
+function springboard_frontend_fieldset($variables) {
+  $element = $variables['element'];
+  element_set_attributes($element, array('id'));
+  _form_set_class($element, array('form-wrapper'));
+
+  $output = '<div' . drupal_attributes($element['#attributes']) . '>';
+  if (!empty($element['#title'])) {
+    $output .= '<div class="div-title">' . $element['#title'] . '</div>';
+  }
+  if (!empty($element['#description'])) {
+    $output .= '<div class="div-description">' . $element['#description'] . '</div>';
+  }
+  $output .= $element['#children'];
+  if (isset($element['#value'])) {
+    $output .= $element['#value'];
+  }
+  $output .= "</div>\n";
+  return $output;
+}
+
+/** 
+ * Overrides theme_checkboxes()
+ */
+function springboard_frontend_checkboxes($variables) {
+  $element = $variables['element'];
+  $attributes = array();
+  if (isset($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+  if (isset($element['#attributes']['title'])) {
+    $attributes['title'] = $element['#attributes']['title'];
+  }
+  return '<div' . drupal_attributes($attributes) . '>' . (!empty($element['#children']) ? $element['#children'] : '') . '</div>';
+}
+
+/** 
+ * Overrides theme_date()
+ */
+function springboard_frontend_date($variables) {
+  $element = $variables['element'];
+
+  $attributes = array();
+  if (isset($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+  if (!empty($element['#attributes']['class'])) {
+    $attributes['class'] = (array) $element['#attributes']['class'];
+  }
+
+  return '<div' . drupal_attributes($attributes) . '>' . drupal_render_children($element) . '</div>';
+}
+
+/** 
+ * Overrides theme_file()
+ */ 
+function springboard_frontend_file($variables) {
+  $element = $variables['element'];
+  $element['#attributes']['type'] = 'file';
+  element_set_attributes($element, array('id', 'name', 'size'));
+  //_form_set_class($element, array('form-file'));
+
+  return '<input' . drupal_attributes($element['#attributes']) . ' />';
+} 
+
+/** 
+ * Overrides theme_password()
+ */ 
+function springboard_frontend_password($variables) {
+  $element = $variables['element'];
+  $element['#attributes']['type'] = 'password';
+  element_set_attributes($element, array('id', 'name', 'size', 'maxlength'));
+  //_form_set_class($element, array('form-text'));
+
+  return '<input' . drupal_attributes($element['#attributes']) . ' />';
+}
+ 
+/** 
+ * Overrides theme_radio()
+ */
+function springboard_frontend_radio($variables) {
+  $element = $variables['element'];
+  $element['#attributes']['type'] = 'radio';
+  element_set_attributes($element, array('id', 'name','#return_value' => 'value'));
+
+  if (isset($element['#return_value']) && $element['#value'] !== FALSE && $element['#value'] == $element['#return_value']) {
+    $element['#attributes']['checked'] = 'checked';
+  }
+  //_form_set_class($element, array('form-radio'));
+
+  return '<input' . drupal_attributes($element['#attributes']) . ' />';
+} 
+ 
