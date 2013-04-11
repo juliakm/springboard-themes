b266e4fee9d1a75cae9f5be811ade31673b4b698
Sheena Donnelly <sheena.donnelly@gmail.com>
2013-04-11 09:52:26 -0400

    t342 - overriding theme_status_messages() to add bootstrap classes for alerts

--- springboard_frontend/template.php
+++ springboard_frontend/template.php
@@ -348,3 +348,42 @@ function springboard_frontend_container($variables) {
 
   return $element['#children'];
 }
+
+/**
+ * Overrdies theme_status_messages().
+ * Add bootstrap's message classes to messages.
+ */
+
+function springboard_frontend_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    
+    if($type == 'status'){ $type = 'success'; }
+    
+    $output .= "<div class=\"alert alert-$status \">\n";
+    // bootstrap dismiss button
+    $output .= "<button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>\n";
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+    $output .= "</div>\n";
+  }
+  return $output;
+} 
