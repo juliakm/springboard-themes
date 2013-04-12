c8d14b5227683cc7d8d9ec3da74913f70381f189
Patrick O'Brien <obrien317@gmail.com>
2013-04-12 11:21:44 -0500

    t341 - adding validation updates

--- springboard_frontend/js/script.js
+++ springboard_frontend/js/script.js
@@ -1,18 +1,18 @@
 (function($) {
   Drupal.behaviors.springboardForms = {
     attach: function (context, settings) {
-         
+      
 	  $(window).ready(function(){
 	    // Validate form
  		$('.webform-client-form').validate({
   		  highlight: function(element) {
     		$(element).closest('.control-group').removeClass('success').addClass('error');
   		  },
-  		  success: function(element) {
-    		element.text('OK!').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
-  		  }
+  		  success: 
+  		    function(element) {
+    		  element.text('OK').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
+  		    }  		  
  		});
-  	  
   	    // CVV custom validation rule
 		$("#edit-submitted-payment-information-payment-fields-credit-card-cvv").rules("add", { 
 	  	  required: true, 
@@ -20,15 +20,16 @@
 	  	  minlength:3, 
 	  	  maxlength:4, 
 	  	  messages: { 
-	    	required: "yes", 
+	    	required: "This field is required", 
 	    	number: "must be an integer",
 	    	minlength: "min of 3 chars",
 	    	maxlength: "max of 4 chars"
 	  	  }
 		});
+		// Other rules to go here
   	  
   	  });
-
-    }
-  }
+  	  
+    } // attach
+  } // Drupal.behaviors
 })(jQuery);
