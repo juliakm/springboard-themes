3dbb20e5f8c7a5a6717c7936b2f2788304be6868
Danny Englander <highrockmedia@gmail.com>
2013-12-04 17:38:39 -0800

    t571 - misc theming.

--- springboard_backend/js/script.js
+++ springboard_backend/js/script.js
@@ -59,10 +59,6 @@
       // Remove Boostrap button class.
       $('input').removeClass('btn');
 
-      // Add odd / even to springboard panes
-      $(".springboard-pane:visible:even").addClass("even");
-      $(".springboard-pane:visible:odd").addClass("odd");
-
     } // attach.function
   } // drupal.behaviors
 })(jQuery);
