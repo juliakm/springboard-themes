ed6b389861cdb7b0994894a74661f62dde45743b
Federico Castaneda <federico.castaneda@jacksonriver.com>
2014-01-15 15:10:58 -0500

    t37 removing the hardcoded open graph protocol code from the template file.

--- springboard_base/html.tpl.php
+++ springboard_base/html.tpl.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie ie7"> <![endif]-->
-<!--[if IE 8]>         <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie ie8"> <![endif]-->
-<!--[if IE 9]>         <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js ie ie9"> <![endif]-->
-<!--[if gt IE 9]><!--> <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://developers.facebook.com/schema/" itemscope itemtype="http://schema.org/Article" class="no-js"> <!--<![endif]-->
   <head profile="<?php print $grddl_profile; ?>">
     <meta name="HandheldFriendly" content="TRUE" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
