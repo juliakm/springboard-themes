601ed01c5eb98680926beeb91794c0d51bc12cf4
Danny Englander <highrockmedia@gmail.com>
2014-03-21 07:34:26 -0700

    t779 - removing dsm

--- springboard_backend/template.php
+++ springboard_backend/template.php
@@ -84,12 +84,3 @@ function springboard_backend_css_alter(&$css) {
     }
   }
 }
-
-function springboard_backend_form_alter(&$form, &$form_state, $form_id) {
-
-//  dsm($form_id);
-//  dsm($form);
-
-}
-
-
