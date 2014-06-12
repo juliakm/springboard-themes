e45a854ba3a5d1de87a3bd49a0753b5c2b94cff6
Danny Englander <highrockmedia@gmail.com>
2014-06-12 11:52:03 -0700

    t890 - theming sbv_notes

---
+++ springboard_backend/templates/views-view-fields--sbv-notes--block.tpl.php
@@ -0,0 +1,15 @@
+
+
+<span class="notes-date-wrapper">
+<?php print $fields['timestamp_1']->content ?>
+</span>
+
+
+<span class="notes-content">
+<?php print $fields['title']->content ?>
+<?php print $fields['description']->content ?>
+<?php print $fields['link']->content ?>
+</span>
+
+
+
